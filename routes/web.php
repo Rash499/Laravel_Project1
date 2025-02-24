468ddb5e28b164cbd117d523d9660fc261236154
Rash499 <rashmikadilmin499@gmail.com>
2025-02-24 23:32:51 +0530

    Note Contoller functions calling

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.show');
+Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
